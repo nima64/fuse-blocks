14f84399913faf5299bcef4713ba3b5561f0adbd
nyima tamang <kennethe407@gmail.com>
2021-05-06 20:52:18 +0545

    Revert "removed comment on plugin file"
    This reverts commit 8d8768f32339e00eb8181c9ca76d766e5d83dd22, reversing
    changes made to 247de16f4ab716356c6e2b75b3e7ce3285d96263.

--- fusedesk-blocks/src/my-cases.asset.php
+++ fusedesk-blocks/src/my-cases.asset.php
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 <?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-is-shallow-equal', 'wp-keycodes', 'wp-polyfill', 'wp-primitives'), 'version' => 'c1a0a63e3a7b79aba79caafbe2a64bfa');
-=======
-<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-is-shallow-equal', 'wp-keycodes', 'wp-polyfill', 'wp-primitives'), 'version' => '4f326d3ee43e728be1fc082c2c72feb5');
->>>>>>> e00e65c1530eee6b0926cbdd83d35742b707d04f
